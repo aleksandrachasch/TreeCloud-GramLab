6be21d3f554f14ba1051832b9b5f83ba6edd9562
aleksandrachasch <chaschina.aleksandra@gmail.com>
2016-07-18 23:59:52 +0300

    Main class of the plugin

---
+++ src/tree/Main.java
@@ -0,0 +1,31 @@
+package tree;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import treecloud.EqualAngle;
+
+/**
+ * Main class of the TreeCloud plugin
+ * (currently works only on trees from TestData)
+ * @author Aleksandra Chashchina
+ *
+ */
+
+public class Main {
+	
+	public static void main(String [] args) throws IOException{
+		
+		TestData test = new TestData();
+		ArrayList<TreeNode> tree = test.getFourthTest();
+		
+		EqualAngle ea = new EqualAngle();
+		
+		ea.doEqualAngle(tree, test.getNtaxaFourth());
+		
+		TreeSVG.drawTreeCloud(tree);
+		
+		
+	}
+
+}
